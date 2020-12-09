2c5a23ad98aacc8fbe96b2d77840a0398d5311f3
Capelaum <thecapellett@gmail.com>
2020-12-09 09:39:20 -0300

    Update reta.go

--- geometria/reta.go
+++ geometria/reta.go
@@ -4,20 +4,21 @@ import "math"
 
 // Ponto representa uma coordenada no plano cartesiano
 type Ponto struct {
-	x float64
-	y float64
+	X float64
+	Y float64
 }
 
-func catetos(a, b Ponto) (cx, cy float64) {
-	cx = math.Abs(b.x - a.x)
-	cy = math.Abs(b.y - a.y)
+//Catetos pega os modulos dos catetos do triangulo formado por 2 pontos
+func Catetos(pontoA, pontoB Ponto) (cx, cy float64) {
+	cx = math.Abs(pontoB.X - pontoA.X)
+	cy = math.Abs(pontoB.Y - pontoA.Y)
 
 	return 
 }
 
 // Distancia linear entre 2 pontos
-func Distancia(a,b Ponto) float64 {
-	cx, cy := catetos(a, b)
-	return math.Sqrt(math.Pow(cx, 2) + math.Pow(cy, 2))
+func Distancia(pontoA,pontoB Ponto) float64 {
+	catetoX, catetoY := Catetos(pontoA, pontoB)
+	return math.Sqrt(math.Pow(catetoX, 2) + math.Pow(catetoY, 2))
 }
 
