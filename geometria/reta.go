e20d0d546c45ba5cc55765c6ca22a3ddad6251be
Capelaum <thecapellett@gmail.com>
2020-12-09 09:03:14 -0300

    pacote geometrica adicionado

---
+++ geometria/reta.go
@@ -0,0 +1,23 @@
+package geometria
+
+import "math"
+
+// Ponto representa uma coordenada no plano cartesiano
+type Ponto struct {
+	x float64
+	y float64
+}
+
+func catetos(a, b Ponto) (cx, cy float64) {
+	cx = math.Abs(b.x - a.x)
+	cy = math.Abs(b.y - a.y)
+
+	return 
+}
+
+// Distancia linear entre 2 pontos
+func Distancia(a,b Ponto) float64 {
+	cx, cy := catetos(a, b)
+	return math.Sqrt(math.Pow(cx, 2) + math.Pow(cy, 2))
+}
+
